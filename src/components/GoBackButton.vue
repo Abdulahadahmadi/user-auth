e726b7216f45e860424d8c68273b42f9d7326b4f
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-08-24 14:58:32 +0430

    go back button for going back from a page added

---
+++ src/components/GoBackButton.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <butto @click="goBack"></butto>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        goBack() {
+            return this.$route.go(-1)
+        }
+    }
+}
+</script>
