0f70014d18a901a5e8da6f74b393c8db430ed174
Abdul Ahad Ahmadi <aahmadi@netlinks.af>
2022-08-25 15:45:24 +0430

    footer and some code for firebase added

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -73,13 +73,6 @@
                     >Posts</router-link
                 >
                 </li>
-                <li>
-                <router-link
-                    to="/register"
-                    class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ring-1 dark:border-gray-700"
-                    >Register</router-link
-                >
-                </li>
             </ul>
             </div>
         </div>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import 'flowbite';
 import  router from './router';
-
+import Vuex from 'vuex';
 
 
 createApp(App).use(router).mount('#app')
