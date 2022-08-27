1b9511bdbb9842a19b36eb5b747a374a395cb83f
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-08-28 00:07:24 +0430

    logout button added and some functionality added

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -15,11 +15,21 @@
             </router-link>
             <div class="flex md:order-2">
             <router-link
+            v-show="!isAuthenticated"
             to="/login"
                 type="button"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
                 Login
+            </router-link>
+            <router-link
+            to="/logout"
+            v-show="isAuthenticated"
+            @click="logout"
+                type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+                Logout
             </router-link>
             <button
                 data-collapse-toggle="navbar-cta"
@@ -73,15 +83,43 @@
                     >Posts</router-link
                 >
                 </li>
+                
+                <li>
+                <router-link
+                v-show="isAuthenticated"
+                    to="/account"
+                    class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >Account</router-link
+                >
+                </li>
             </ul>
             </div>
         </div>
         </nav>
+        <div v-show="isAuthenticated" class="ring-1 w-2/6 bg-gray-800 text-white flex my-10 py-5 ">
+                <h2 class="text-xl font-bold">User Profile</h2>
+                <pre>
+                    <img class="rounded-full shadow-xl" src="https://lh3.googleusercontent.com/a-/AFdZucotgLAz3E5ybq2LoZbWs2cbn0wT8b-U56f3G5Hb=s96-c" alt="profile" />
+                    <p>{{ user.name }}</p>
+                    <p>{{ user.email }}</p>
+                </pre>
+            </div>
     </div>
+            
 </template>
 <script>
+import { useAuth0 } from '@auth0/auth0-vue';
 export default {
-    
+    setup() {
+        const { logout, loginWithRedirect, user, isAuthenticated }  = useAuth0();
+        return {
+            logout: () => {
+                logout({ returnTo: window.location.origin });
+            },
+            user,
+            isAuthenticated,
+        }
+    }
 }
 </script>
 <style lang="">
