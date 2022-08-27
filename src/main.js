1aa19545aca4eac766dcd35494e6df8de1943837
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-08-28 00:06:58 +0430

    imported auth0 for project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import 'flowbite';
-import  router from './router';
-// import Vuex from 'vuex';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "flowbite";
+import router from "./router";
+import { createAuth0 } from "@auth0/auth0-vue";
 
-// const store = createStore({
-    
-// })
+// App.use();
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(
+        createAuth0({
+        domain: "ahad-dev.au.auth0.com",
+        client_id: "OEFteWCxyFJtAHHL5oZXxVXEPaCha8Pw",
+        redirect_uri: window.location.origin,
+        })
+    )
+    .mount("#app");
